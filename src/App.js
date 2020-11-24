aaf719bb266769fafc9e1e787f916556b6dc4b2d
tichambers99 <khanhkhanhars@gmail.com>
2020-11-24 19:50:45 +0700

    render a list

--- src/App.js
+++ src/App.js
@@ -20,12 +20,43 @@ function App() {
   //     </header>
   //   </div>
   // );
+
+  let fixtures = [
+    {
+      home: {
+        name: 'Arsenal',
+        logo: 'https://cdn.sportdataapi.com/images/soccer/teams/100/18.png',
+        goal: 5
+      },
+      away: {
+        name: 'Manchester United',
+        logo: 'https://cdn.sportdataapi.com/images/soccer/teams/100/19.png',
+        goal: 1
+      }
+    },
+    {
+      home: {
+        name: 'Watford',
+        logo: 'https://cdn.sportdataapi.com/images/soccer/teams/100/11.png',
+        goal: 2
+      },
+      away: {
+        name: 'Brighton&Hole Albilon',
+        logo: 'https://cdn.sportdataapi.com/images/soccer/teams/100/12.png',
+        goal: 1
+      }
+    }
+  ]
     
   return(
     <div className="App">
       <h1>Ket qua thi dau vong 37 Ngoai Hang Anh</h1>
-      <Match logo1 = 'https://cdn.sportdataapi.com/images/soccer/teams/100/18.png' logo2 = 'https://cdn.sportdataapi.com/images/soccer/teams/100/19.png'/>
-      <Match logo1 = 'https://cdn.sportdataapi.com/images/soccer/teams/100/11.png' logo2 = 'https://cdn.sportdataapi.com/images/soccer/teams/100/12.png'/>
+      {/* <Match logo1 = 'https://cdn.sportdataapi.com/images/soccer/teams/100/18.png' logo2 = 'https://cdn.sportdataapi.com/images/soccer/teams/100/19.png'/>
+      <Match logo1 = 'https://cdn.sportdataapi.com/images/soccer/teams/100/11.png' logo2 = 'https://cdn.sportdataapi.com/images/soccer/teams/100/12.png'/> */}
+
+      {
+        fixtures.map((fixture) => <Match logo1={fixture.home.logo} goal1={fixture.home.goal} logo2={fixture.away.logo} goal2={fixture.away.goal}/>)
+      }
     </div>
   )
 }
--- src/components/Match.js
+++ src/components/Match.js
@@ -4,9 +4,10 @@ import React, {Component} from 'react';
 class Match extends Component{
     render() {
         return(
-            <div className="Match">
-                <img src= {this.props.logo1} alt='Logo'></img>
-                <img src= {this.props.logo2} alt='Logo'></img>
+            <div className="Match" > 
+                <img src= {this.props.logo1} alt='Logo' style = {{height: '50%'}}></img>
+                <span style = {{fontSize: '30px'}}>{this.props.goal1} - {this.props.goal2}</span>
+                <img src= {this.props.logo2} alt='Logo' style = {{height: '50%'}}></img>
             </div>
             )
     }
