c9bca5abfac31ca39dd69c5914b91fe44edb6a34
tichambers99 <khanhkhanhars@gmail.com>
2020-11-24 23:26:15 +0700

    fetch db, undefined map()

--- src/App.js
+++ src/App.js
@@ -1,64 +1,58 @@
 import Match from './components/Match';
-import arsenal from './logo.svg'
 import './App.css';
+import axios from 'axios';
+import {Component} from 'react';
 
-function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //       >
-  //         Hello world
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { fixtures: {} };
+  }
 
-  let fixtures = [
-    {
-      home: {
-        name: 'Arsenal',
-        logo: 'https://cdn.sportdataapi.com/images/soccer/teams/100/18.png',
-        goal: 5
-      },
-      away: {
-        name: 'Manchester United',
-        logo: 'https://cdn.sportdataapi.com/images/soccer/teams/100/19.png',
-        goal: 1
-      }
-    },
-    {
-      home: {
-        name: 'Watford',
-        logo: 'https://cdn.sportdataapi.com/images/soccer/teams/100/11.png',
-        goal: 2
-      },
-      away: {
-        name: 'Brighton&Hole Albilon',
-        logo: 'https://cdn.sportdataapi.com/images/soccer/teams/100/12.png',
-        goal: 1
-      }
+  async componentDidMount() {
+    const response = await fetch(`https://api.npoint.io/9329b911b166f25cbef3`);
+    const json = await response.json();
+    this.setState({ fixtures: json });
+    console.log(this.state.fixtures.data);
+    console.log(typeof(this.state.fixtures.data));
+  }
+
+  render(){
+    return(
+      
+      <div className="App">
+        <h1>Ket qua thi dau vong 37 Ngoai Hang Anh</h1>
+        {
+          //this.state.fixtures.data.map((fixture, index) => <Match key = {index} logo1={fixture.home_team.logo} logo2={fixture.away_team.logo}/>)
+        }
+      </div>
+    )
     }
-  ]
-    
-  return(
-    <div className="App">
-      <h1>Ket qua thi dau vong 37 Ngoai Hang Anh</h1>
-      {/* <Match logo1 = 'https://cdn.sportdataapi.com/images/soccer/teams/100/18.png' logo2 = 'https://cdn.sportdataapi.com/images/soccer/teams/100/19.png'/>
-      <Match logo1 = 'https://cdn.sportdataapi.com/images/soccer/teams/100/11.png' logo2 = 'https://cdn.sportdataapi.com/images/soccer/teams/100/12.png'/> */}
-
-      {
-        fixtures.map((fixture) => <Match logo1={fixture.home.logo} goal1={fixture.home.goal} logo2={fixture.away.logo} goal2={fixture.away.goal}/>)
-      }
-    </div>
-  )
 }
 
 export default App;
+
+
+
+ // componentDidMount() {
+  //   fetch(`https://api.coinmarketcap.com/v1/ticker/?limit=10`)
+  //     .then(res => res.json())
+  //     .then(json => this.setState({ data: json }));
+
+  //   // axios.get('https://api.npoint.io/9329b911b166f25cbef3').then(function (response) {
+  //   //   // handle success
+  //   //   //console.log(response.data);
+  //   //   fixtures = response.data.data;
+  //   //   console.log(fixtures);
+  //   // })
+  // }
+
+  // render(){
+  // var fixtures = [];
+  // <div className="App">
+  //   <h1>Ket qua thi dau vong 37 Ngoai Hang Anh</h1>
+  //   {
+  //     fixtures.map((fixture, index) => <Match key = {index} logo1={fixture.home_team.logo} logo2={fixture.away_team.logo}/>)
+  //   }
+  // </div>
+  // }
